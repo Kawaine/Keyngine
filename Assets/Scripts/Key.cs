e27436709ad35a3cd555ec44989a6ef844f3a75a
Kyle Nestmann <nestmann36@gmail.com>
2020-04-07 07:46:44 -0400

    Added Unity project.

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kawaine.KeyboardEngine
+{
+    public class Key : MonoBehaviour
+    {
+        public KeyCode keyCode;
+        [Header("References")]
+        public KeyTrigger keyTrigger;
+        //public Key
+    }
+}
+
---
+++ Assets/Scripts/KeyboardManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardManager : MonoBehaviour
+{
+    public enum KeyboardType { TwoD, ThreeD, Isometric }
+    [Tooltip("Would you like key names turned on?")] public bool keyNames;
+}
